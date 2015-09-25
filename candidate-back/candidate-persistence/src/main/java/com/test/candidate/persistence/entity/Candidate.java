5e58a30cd8e38c7e972e4ae28f234c613e6aaff2
Benoit Piette <bpiette@gmail.com>
2015-09-25 20:19:27 +0200

    * Minimal Candidate entity defined

--- candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/entity/Candidate.java
+++ candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/entity/Candidate.java
@@ -1,9 +1,44 @@
 package com.test.candidate.persistence.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
 /**
  * Created by oleg on 09/08/15.
  */
-//TODO map entity using JPA annotations
-public class Candidate {
+@Entity
+public class Candidate implements Serializable {
 
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Size(max = 30)
+    private String name;
+
+    private boolean enabled;
+
+    Candidate() {
+    }
+
+    public Candidate(String name, boolean enabled) {
+        this.name = name;
+        this.enabled = enabled;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
+
