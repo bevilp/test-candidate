a3dce1cc2e1d3b46589f6a9855bd76d61b66e242
Benoit Piette <bpiette@gmail.com>
2015-09-25 23:30:20 +0200

    * CandidateRepository

--- candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/repository/CandidateRepository.java
+++ candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/repository/CandidateRepository.java
@@ -1,9 +1,14 @@
 package com.test.candidate.persistence.repository;
 
+import com.test.candidate.persistence.entity.Candidate;
+import org.springframework.stereotype.Repository;
+
 /**
+ * Candidate repository
+ * <p>
  * Created by oleg on 09/08/15.
  */
-//TODO create CandidateRepository interface
-public interface CandidateRepository {
+@Repository
+public interface CandidateRepository extends BaseRepository<Candidate, Integer> {
 
 }
