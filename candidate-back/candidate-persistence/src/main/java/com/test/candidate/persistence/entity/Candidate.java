b325375cb001b9821e03ce1cc133498aedfdb943
Benoit Piette <bpiette@gmail.com>
2015-09-25 23:26:14 +0200

    * Now using only JPA annotations. * Added serialVersionUID

--- candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/entity/Candidate.java
+++ candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/entity/Candidate.java
@@ -1,9 +1,9 @@
 package com.test.candidate.persistence.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -12,11 +12,13 @@ import java.io.Serializable;
 @Entity
 public class Candidate implements Serializable {
 
+    private static final long serialVersionUID = 1l;
+
     @Id
     @GeneratedValue
     private int id;
 
-    @Size(max = 30)
+    @Column(length = 30)
     private String name;
 
     private boolean enabled;
