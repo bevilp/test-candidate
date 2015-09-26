f995c7f7ccb87f97175e032cde7be5ec686e3573
Benoit Piette <bpiette@gmail.com>
2015-09-26 12:07:21 +0200

    * implemented new update and create candidate methods

--- candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateService.java
+++ candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateService.java
@@ -1,5 +1,7 @@
 package com.test.candidate.service;
 
+import com.test.candidate.service.controller.CandidateForm;
+
 import java.util.List;
 
 /**
@@ -7,5 +9,27 @@ import java.util.List;
  */
 public interface CandidateService {
 
+    /**
+     * Get a list of all candidates
+     *
+     * @return
+     */
     List<CandidateDto> getAllCandidates();
+
+    /**
+     * Update a candidate
+     *
+     * @param id
+     * @param candidateForm
+     * @return
+     */
+    CandidateDto updateCandidate(int id, CandidateForm candidateForm);
+
+    /**
+     * Create a new candidate
+     *
+     * @param candidateForm
+     * @return
+     */
+    CandidateDto createCandidate(CandidateForm candidateForm);
 }
--- candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateServiceImpl.java
+++ candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateServiceImpl.java
@@ -2,10 +2,13 @@ package com.test.candidate.service;
 
 import com.test.candidate.persistence.entity.Candidate;
 import com.test.candidate.persistence.repository.CandidateRepository;
+import com.test.candidate.service.controller.CandidateForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -36,5 +39,27 @@ public class CandidateServiceImpl implements CandidateService {
                 .stream()
                 .map(CANDIDATE_TO_CANDIDATE_VO_MAPPER)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public CandidateDto updateCandidate(int id, @NotNull CandidateForm candidateForm) {
+        Assert.notNull(candidateForm);
+        Candidate candidate = candidateRepository.findOne(id);
+        if (candidate == null) {
+            //TODO throw exception
+        }
+
+        candidate.setName(candidateForm.getName());
+        candidate.setEnabled(candidateForm.isEnabled());
+        candidate = candidateRepository.save(candidate);
+        return CANDIDATE_TO_CANDIDATE_VO_MAPPER.apply(candidate);
+    }
+
+    @Override
+    public CandidateDto createCandidate(@NotNull CandidateForm candidateForm) {
+        Assert.notNull(candidateForm);
+        Candidate candidate = new Candidate(candidateForm.getName(), candidateForm.isEnabled());
+        candidate = candidateRepository.save(candidate);
+        return CANDIDATE_TO_CANDIDATE_VO_MAPPER.apply(candidate);
     }
 }
