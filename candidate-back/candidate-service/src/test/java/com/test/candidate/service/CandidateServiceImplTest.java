3660b8fa46b12232c86f9be374bba3a8ad90393e
Benoit Piette <bpiette@gmail.com>
2015-09-26 15:43:38 +0200

    * CandidateService tests

--- candidate-back/candidate-service/src/test/java/com/test/candidate/service/CandidateServiceImplTest.java
+++ candidate-back/candidate-service/src/test/java/com/test/candidate/service/CandidateServiceImplTest.java
@@ -2,16 +2,20 @@ package com.test.candidate.service;
 
 import com.test.candidate.persistence.entity.Candidate;
 import com.test.candidate.persistence.repository.CandidateRepository;
+import com.test.candidate.service.controller.CandidateForm;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * candidate
@@ -29,7 +33,6 @@ public class CandidateServiceImplTest {
     private CandidateRepository candidateRepository;
 
     private final List<Candidate> candidates = new ArrayList<>();
-
     {
         candidates.add(new Candidate("john", true));
         candidates.add(new Candidate("alex", true));
@@ -39,11 +42,51 @@ public class CandidateServiceImplTest {
     @Test
     public void findAllCandidatesTest() {
         // we expect the repository to be called and to return the candidates list
-        Mockito.when(candidateRepository.findAll()).thenReturn(candidates);
+        when(candidateRepository.findAll()).thenReturn(candidates);
 
         List<CandidateDto> candidates = candidateService.getAllCandidates();
 
-        Assert.assertNotNull(candidates);
+        assertNotNull(candidates);
         Assert.assertEquals(3, candidates.size());
+    }
+
+    @Test
+    public void insertCandidateTest() {
+        when(candidateRepository.save(any(Candidate.class))).thenReturn(new Candidate("Ben", false));
+
+        CandidateDto candidateDto = candidateService.createCandidate(new CandidateForm("Ben", false));
+
+        assertNotNull(candidateDto);
+        assertEquals("Ben", candidateDto.getName());
+        assertFalse(candidateDto.isEnabled());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertCandidateNullFormTest() {
+        candidateService.createCandidate(null);
+    }
+
+    @Test
+    public void updateCandidateTest() throws EntityNotFoundException {
+        int candidateId = 1;
+        Candidate candidate = new Candidate("Benoit", true);
+        when(candidateRepository.findOne(candidateId)).thenReturn(candidate);
+
+        Candidate updatedCandidate = new Candidate("Ben", false);
+        when(candidateRepository.save(any(Candidate.class))).thenReturn(updatedCandidate);
+
+        CandidateDto candidateDto = candidateService.updateCandidate(1, new CandidateForm("Ben", false));
+
+        assertNotNull(candidateDto);
+        assertEquals("Ben", candidateDto.getName());
+        assertFalse(candidateDto.isEnabled());
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void updateNonExistingCandidateTest() throws EntityNotFoundException {
+        int candidateId = 10;
+        when(candidateRepository.findOne(candidateId)).thenReturn(null);
+
+        candidateService.updateCandidate(candidateId, new CandidateForm("test", false));
     }
 }
