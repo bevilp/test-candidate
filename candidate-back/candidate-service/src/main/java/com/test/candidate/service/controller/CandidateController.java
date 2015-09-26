7b359e26c9bb9f3be5b0337cafa647eda2e27d73
Benoit Piette <bpiette@gmail.com>
2015-09-26 10:58:12 +0200

    * CandidateController implementation

--- candidate-back/candidate-service/src/main/java/com/test/candidate/service/controller/CandidateController.java
+++ candidate-back/candidate-service/src/main/java/com/test/candidate/service/controller/CandidateController.java
@@ -1,15 +1,37 @@
 package com.test.candidate.service.controller;
 
+import com.test.candidate.service.CandidateDto;
+import com.test.candidate.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
+ * Candidate controller
+ * <p>
  * Created by oleg on 12/08/15.
  */
 @RestController
 @RequestMapping("/candidate")
-//TODO
 public class CandidateController {
 
+    private final CandidateService candidateService;
 
+    @Autowired
+    public CandidateController(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
+    /**
+     * Returns a list of all candidates
+     *
+     * @return list of all candidates
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public List<CandidateDto> findAll() {
+        return candidateService.getAllCandidates();
+    }
 }
