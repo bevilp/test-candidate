7efe1fcf15c729a449ec6158a8500d1b6e57706f
Benoit Piette <bpiette@gmail.com>
2015-09-26 23:50:38 +0200

    * Missing field constraints

--- candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/entity/Candidate.java
+++ candidate-back/candidate-persistence/src/main/java/com/test/candidate/persistence/entity/Candidate.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -19,6 +21,8 @@ public class Candidate implements Serializable {
     private int id;
 
     @Column(length = 30)
+    @NotNull
+    @Size(min = 1, max = 30)
     private String name;
 
     private boolean enabled;
