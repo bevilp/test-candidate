55026dc14d5905278bc526845b78bd4d70c1ca07
Benoit Piette <bpiette@gmail.com>
2015-09-28 00:34:10 +0200

    * Candidate service now uses the Eureka server to discover the intake service

--- candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateServiceApp.java
+++ candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateServiceApp.java
@@ -3,13 +3,15 @@ package com.test.candidate.service;
 import com.test.candidate.persistence.PersistenceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 /**
  * Created by oleg on 07/08/15.
  */
 @SpringBootApplication
-@Import({CandidateServiceAppConfiguration.class, PersistenceConfiguration.class})
+@EnableDiscoveryClient
+@Import({PersistenceConfiguration.class})
 public class CandidateServiceApp {
 
     public static void main(String[] args) {
--- candidate-back/candidate-service/src/main/java/com/test/candidate/service/IntakeGenerationNotificationServiceImpl.java
+++ candidate-back/candidate-service/src/main/java/com/test/candidate/service/IntakeGenerationNotificationServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,24 +13,25 @@ import org.springframework.web.client.RestTemplate;
  * @author Ben
  * @since 27/09/2015
  */
+@Service
 public class IntakeGenerationNotificationServiceImpl implements IntakeGenerationNotificationService {
 
     private static final Logger LOG = LoggerFactory
             .getLogger(CandidateServiceImpl.class);
 
-    private final String intakeServiceNotificationUrl;
+    // this should be moved to application.yml
+    private static final String INTAKE_SERVICE_NOTIFICATION_URL = "http://CANDIDATE-INTAKE-GENERATION-SERVICE/intake-generation-service/notify";
 
     private final RestTemplate restTemplate;
 
     @Autowired
-    public IntakeGenerationNotificationServiceImpl(String intakeServiceNotificationUrl, RestTemplate restTemplate) {
-        this.intakeServiceNotificationUrl = intakeServiceNotificationUrl;
+    public IntakeGenerationNotificationServiceImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
     public void notify(int id) {
-        ResponseEntity<String> response = restTemplate.postForEntity(intakeServiceNotificationUrl, id, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(INTAKE_SERVICE_NOTIFICATION_URL, id, String.class);
         if (response.getStatusCode().is2xxSuccessful()) {
             LOG.info("successfully notified intake generation service");
         } else {
