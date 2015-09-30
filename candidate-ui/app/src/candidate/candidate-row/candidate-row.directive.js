eb07f2945239bd5372e0eadc508baf240a524153
Benoit Piette <bpiette@gmail.com>
2015-09-30 23:11:35 +0200

    Step 2

--- candidate-ui/app/src/candidate/candidate-row/candidate-row.directive.js
+++ candidate-ui/app/src/candidate/candidate-row/candidate-row.directive.js
@@ -1,19 +1,44 @@
 (function () {
     'use strict';
 
-    angular
-        .module('app.candidate')
-        .directive('candidateRow', [
-            CandidateRow
-        ]);
+    angular.module('app.candidate').directive('candidateRow', [
+        CandidateRow
+    ]);
+
+    CandidateRowController.$inject = ['CandidateService', '$log'];
 
     //directive
     function CandidateRow() {
         return {
+            bindToController: true,
+            controller: CandidateRowController,
+            controllerAs: 'ctrl',
             restrict: 'A',
             scope: {candidate: '='},
             templateUrl: './src/candidate/candidate-row/candidate-row.html'
         };
     }
 
+    function CandidateRowController(CandidateService, $log) {
+        var self = this;
+
+        self.enabledCheckbox = self.candidate.enabled;
+        self.toggle = toggle;
+
+        function toggle() {
+            // save previous state in case update fails
+            var previousValue = self.candidate.enabled;
+            self.candidate.enabled = self.enabledCheckbox;
+
+            CandidateService.updateCandidate(self.candidate).then(function (response) {
+                // success
+                $log.debug('update succeeded');
+            }, function () {
+                // update failed. Restore previous state
+                $log.error('update failed');
+                self.candidate.enabled = previousValue;
+                self.enabledCheckbox = previousValue;
+            });
+        }
+    }
 })();
--- candidate-ui/app/src/candidate/candidate.service.js
+++ candidate-ui/app/src/candidate/candidate.service.js
@@ -5,16 +5,19 @@
         .module('app.candidate')
         .factory('CandidateService', CandidateService);
 
-    CandidateService.$inject = ['$http', '$log'];
+    CandidateService.$inject = ['$http', '$log', '$q'];
 
-    function CandidateService($http, $log) {
+    function CandidateService($http, $log, $q) {
+        var _candidateUrl = '/candidate';
         var service = {
-            getCandidates: getCandidates
+            getCandidates: getCandidates,
+            updateCandidate: updateCandidate,
+            addCandidate: addCandidate
         };
         return service;
 
         function getCandidates() {
-            return $http.get('/candidate')
+            return $http.get(_candidateUrl)
                 .then(getCandidatesComplete)
                 .catch(getCandidatesFailed);
 
@@ -23,7 +26,42 @@
             }
 
             function getCandidatesFailed(error) {
-                logger.error('XHR Failed for getCandidates.' + error.data);
+                $log.error('XHR Failed for getCandidates.' + error.data);
+            }
+        }
+
+        function updateCandidate(candidate) {
+            return $http.put(_candidateUrl + '/' + candidate.id, candidate)
+                .then(updateCandidateComplete)
+                .catch(updateCandidateFailed);
+            function updateCandidateComplete(response) {
+                $log.debug('candidate updated');
+                $log.debug(response);
+                return response.data;
+            }
+
+            function updateCandidateFailed(error) {
+                $log.error('XHR Failed for updateCandidate.');
+                $log.error(error)
+                return $q.reject(error);
+            }
+        }
+
+        function addCandidate(candidate) {
+            return $http.post(_candidateUrl,
+                candidate)
+                .then(addCandidateComplete)
+                .catch(addCandidateCompleteFailed);
+
+            function addCandidateComplete(response) {
+                $log.debug('Candidate Added ' + response.data);
+                return response.data;
+            }
+
+            function addCandidateCompleteFailed(error) {
+                $log.error('XHR Failed for updateCandidate.');
+                $log.error(error)
+                return $q.reject(error);
             }
         }
     }
